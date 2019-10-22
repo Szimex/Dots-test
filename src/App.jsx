48c2475d0cffbde2fc149d92a3b85ff89e4adc2b
Mateusz Sobczak <sobczak.mateusz@gmail.com>
2019-10-22 13:45:29 +0200

    Use React Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { hot } from 'react-hot-loader/root';
 import HelloWorld from './components/hello-world';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [number, setNumber] = useState(2);
 
-  render() {
-    return <HelloWorld title="Hello from React webpack" />;
-  }
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <HelloWorld number={number} title="Hello from React webpack" />
+      <button type="button" onClick={() => setNumber(number + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
 
-export default hot(App);
+export default hot(Example);
--- src/components/hello-world/index.jsx
+++ src/components/hello-world/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './hello-world.css';
 
-const HelloWorld = ({ title }) => (
-  <div className={style['hello-world']}>{title}</div>
+const HelloWorld = ({ number, title }) => (
+  <div className={style['hello-world']}>
+    {title}
+    <br />
+    Number: {number}, Square: {number ** 2}
+  </div>
 );
 
 HelloWorld.propTypes = {
+  number: PropTypes.number,
   title: PropTypes.string,
 };
 
